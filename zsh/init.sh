71a4efdf22f12fb8867a7eb6e907fb7be274cf7b
Tendayi Kamucheka <ftendayi@gmail.com>
2023-10-25 16:22:28 -0500

    First commit

---
+++ zsh/init.sh
@@ -0,0 +1,60 @@
+##############################################################################
+# PATH config
+##############################################################################
+
+export PATH=$HOME/.local/bin:$PATH
+export XDG_CONFIG_HOME=$HOME/.config
+
+##############################################################################
+
+##############################################################################
+# Powerlevel10k config
+##############################################################################
+#
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+set P10K="${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" 
+[[ -r $P10K ]]  && source $P10K 
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+##############################################################################
+
+##############################################################################
+# Theming
+##############################################################################
+ 
+# Catppuccin syntax highlighting
+source "$XDG_CONFIG_HOME/zsh/themes/catppuccin/catppuccin_mocha-zsh-syntax-highlighting.zsh"
+
+# ZSH Theme
+export CONFIG_ZSH_THEME="powerlevel10k/powerlevel10k"
+
+#############################################################################
+
+#############################################################################
+# Preferred editor for local and remote sessions
+#############################################################################
+
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nvim'
+fi
+
+#############################################################################
+
+#############################################################################
+# User config
+#############################################################################
+
+# ZSH Plugins
+export plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
+
+# Use Neovim
+alias vim="nvim"
+
+#############################################################################
